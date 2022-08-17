f0323e52c15f52daa7e25eef839dea346a2b2671
nayana005 <nayanasuresh1005@gmail.com>
2022-08-17 23:30:46 +0530

    Added: Create Contact

---
+++ Contacts.java
@@ -0,0 +1,107 @@
+package com.bridgelabz.address_book;
+
+public class Contacts {
+	
+	private String firstName;
+    private String lastName;
+    private String address;
+    private String emailId;
+    private String city;
+    private String state;
+    private int zipCode;
+    private long phoneNo;
+
+    public Contacts(String firstName, String lastName, String address, String emailId,
+                    String city, String state, int zipCode, long phoneNo) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.emailId = emailId;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.phoneNo = phoneNo;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public long getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(long phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacts{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", emailId='" + emailId + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zipCode=" + zipCode +
+                ", phoneNo=" + phoneNo +
+                '}';
+    }
+}
+
+
+
